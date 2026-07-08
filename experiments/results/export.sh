adbaba2f6eba6c904cbdc6708d72b0ae3daf3ba8
Hieu Hay Ho <truongxuanhieu11@gmail.com>
2026-07-08 04:11:34 +0000

    update run file and export

--- experiments/results/export.sh
+++ experiments/results/export.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python3 export_cplex-cp.py   ./extended_bound    ./extended_bound/cplex-cp-extended-bound.xlsx
-python3 export_cplex-cp.py   ./reduced_bound     ./reduced_bound/cplex-cp-reduced-bound.xlsx
-python3 export_cplex-cp.py   ./standard_bound    ./standard_bound/cplex-cp-standard-bound.xlsx
+python3 export_cplex-cp.py   ./abp    ./abp/cplex-cp.xlsx
