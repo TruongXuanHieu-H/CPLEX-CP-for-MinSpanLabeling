901035231b21b7a32cdc355d8b352f2179a4672c
Hieu Hay Ho <truongxuanhieu11@gmail.com>
2026-07-15 05:11:31 +0000

    run cabp

--- experiments/results/export.sh
+++ experiments/results/export.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 export_cplex-cp.py   ./abp    ./abp/cplex-cp.xlsx
+python3 export_cplex-cp.py   ./abp    ./abp/cplex-cp-abp.xlsx
+python3 export_cplex-cp.py   ./cabp   ./cabp/cplex-cp-cabp.xlsx
